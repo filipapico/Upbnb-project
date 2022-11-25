44bf7a1f69917691bfce92e3aff5b19db6d8b8f3
filipapico <filipapico@gmail.com>
2022-11-25 18:17:17 +0000

    Added font to general app style.scss Added scss to the navbar component

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,5 +1,7 @@
 import {Component} from '@angular/core';
+import {faSearch} from "@fortawesome/free-solid-svg-icons";
 import {faHeart} from "@fortawesome/free-regular-svg-icons";
+import {faHouse} from "@fortawesome/free-solid-svg-icons";
 import {faHeart as faHeartSolid} from "@fortawesome/free-solid-svg-icons";
 
 @Component({
@@ -8,6 +10,8 @@ import {faHeart as faHeartSolid} from "@fortawesome/free-solid-svg-icons";
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent {
+  faSearch = faSearch;
+  faHouse = faHouse;
   faHeart = faHeart;
   faHeartSolid = faHeartSolid;
 
