3cfa2050c28853a16467aa8fb6c0abd406f1555d
filipapico <filipapico@gmail.com>
2022-11-25 19:47:23 +0000

    Added css to house component

--- src/app/house/house.component.ts
+++ src/app/house/house.component.ts
@@ -1,5 +1,7 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {UpbnbService} from "../upbnb.service";
+import {faHeart} from "@fortawesome/free-regular-svg-icons";
+import {faHeart as faHeartSolid} from "@fortawesome/free-solid-svg-icons";
 
 @Component({
   selector: 'app-house',
@@ -8,10 +10,13 @@ import {UpbnbService} from "../upbnb.service";
 })
 export class HouseComponent implements OnInit {
 
-  @Input() featured_photo! : string
-  @Input() city! : string
-  @Input() country! : string
-  @Input() rating! : number
+  faHeart = faHeart;
+  faHeartSolid = faHeartSolid;
+
+  @Input() featured_photo!: string
+  @Input() city!: string
+  @Input() country!: string
+  @Input() rating!: number
 
   constructor(private upbnb: UpbnbService) {
   }
