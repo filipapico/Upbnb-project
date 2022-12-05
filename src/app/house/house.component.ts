78e6569395093dcf6c199b52e7aab49491c78838
filipapico <filipapico@gmail.com>
2022-12-05 19:37:04 +0000

    Cleaned unnecessary code and reorganized inputs order.

--- src/app/house/house.component.ts
+++ src/app/house/house.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, Input} from '@angular/core';
 import {UpbnbService} from "../upbnb.service";
 import {faHeart} from "@fortawesome/free-regular-svg-icons";
 import {faHeart as faHeartSolid} from "@fortawesome/free-solid-svg-icons";
@@ -9,31 +9,26 @@ import {ActivatedRoute} from "@angular/router";
   templateUrl: './house.component.html',
   styleUrls: ['./house.component.scss']
 })
-export class HouseComponent implements OnInit {
+export class HouseComponent {
   faHeart = faHeart;
   faHeartSolid = faHeartSolid;
 
+  @Input() id!: number
   @Input() featured_photo!: string
   @Input() city!: string
   @Input() country!: string
   @Input() rating!: number
   @Input() price!: number
-  @Input() host_type?: string //? because this may "exist" or not, depending on the component House will be used in...
-  @Input() id!: number
-  @Input() time? : string //? idem
+  @Input() host_type?: string //? because it may "exist" or not, depending on the component House will be used in...
+  @Input() time?: string //? idem
 
-  host_types : any = {
+  // Object created to "convert" a variable to a string
+  host_types: any = {
     professional: "Anfitrião profissional",
     individual: "Anfitrião individual"
   }
-  // This object allows to "convert" a variable to a string
 
   constructor(public upbnbService: UpbnbService, private route: ActivatedRoute) {
     this.id = route.snapshot.params['id_casa'];
   }
-
-  ngOnInit(): void {
-
-  }
-
 }
